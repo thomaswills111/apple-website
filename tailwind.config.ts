53f3e73d03ee294eaf1d53548f9b5bbf34a91a85
Thomas Wills <thomaswills111@gmail.com>
2024-12-28 10:21:59 +0800

    initial commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -9,6 +9,15 @@ export default {
   theme: {
     extend: {
       colors: {
+        blue: "#2997FF",
+        gray: {
+          DEFAULT: "#86868b",
+          100: "#94928d",
+          200: "#afafaf",
+          300: "#42424570",
+        },
+        zinc: "#101010",
+
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
